9e2f37e2d6415108c699d2c7036a5826c9cb4aee
Sheheryar Ali <sheheryar.s.ali@gmail.com>
2017-02-02 18:37:25 -0500

    Correctly pull bots ID

--- print_bot_id.py
+++ print_bot_id.py
@@ -3,17 +3,16 @@ from slackclient import SlackClient
 
 BOT_NAME = 'pabot'
 
-slack_client = SlackClient(os.environ.get(SLACK_BOT_TOKEN'))
-
-
-if __name__ == '__main__':
+slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+print os.environ.get('SLACK_BOT_TOKEN')
+if __name__ == "__main__":
 	api_call = slack_client.api_call("users.list")
 	if api_call.get('ok'):
 		#retrieve all users so bot is findable 
 		users = api_call.get('members')
 		for user in users: 
-			if 'name' in user and user.get('name') = BOT_NAME:
+			if 'name' in user and user.get('name') == BOT_NAME:
 				print("Bot ID for '" + user['name'] + "' is " + user.get('id'))
 
-			else:
-				print("sorry couldn't find the bot with name" + BOT_NAME)
+	else:
+		print("sorry couldn't find the bot with name " + BOT_NAME)
---
+++ set_env.sh
@@ -0,0 +1,4 @@
+#set the environment variables
+
+export SLACK_BOT_TOKEN='xoxb-136450403028-gPaSDwQ2PrXnFiLN9QXEX4Bu'
+export BOT_ID='insert_bot_id'
