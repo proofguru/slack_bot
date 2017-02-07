f2fe9c47414098dfa8c9c1ee7908bb4e7017f9b5
Sheheryar <sheheryar.s.ali@gmail.com>
2017-02-07 16:42:29 -0500

    Add files via upload
    added weather capabilities for Toronto

--- pabot.py
+++ pabot.py
@@ -1,27 +1,46 @@
-#passive_aggressive slack bot v0.1
+#passive_aggressive slack bot v0.2
 
 import os
 import time
 from slackclient import SlackClient
+import openweather
+import pyowm
 
 
-# starterbot's ID as an environment variable
+# ID and WEATHER
 BOT_ID = os.environ.get("BOT_ID")
+OPEN_WEATHER_TOKEN = os.environ.get("OPEN_WEATHER_TOKEN")
 
 # constants
 AT_BOT = "<@" + BOT_ID + ">"
 
-# instantiate Slack & Twilio clients
+
+# instantiate Slack
 slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 
+#url for weather
+owm = pyowm.OWM(OPEN_WEATHER_TOKEN)
 
 def handle_command(command, channel):
-    if 'help' in command:
-        response = "Hey man why don't you help yourself?"
+    if 'weather' in command:
+        w_dict = weather_grab()
+        weather = str(w_dict.get('temp'))
+        response = "The weather in Toronto is " + weather + " right now REEEE. Maybe look outside?"
+        print response
+    
+    
+    elif 'help' in command:
+        response = "Hey man why don't you help yourself? Pro tip: Ask me @pabot what is the weather"
     else:
         response = "Sorry man, '" + command + "' is offensive to me"
     slack_client.api_call("chat.postMessage", channel=channel,
-        text=response, as_user=True)
+                          text=response, as_user=True)
+
+#grabs the weather
+def weather_grab():
+    observation = owm.weather_at_place('Toronto, CA')
+    weather = observation.get_weather()
+    return weather.get_temperature('celsius')
 
 
 
@@ -30,14 +49,14 @@ def parse_slack_output(slack_rtm_output):
         The Slack Real Time Messaging API is an events firehose.
         this parsing function returns None unless a message is
         directed at the Bot, based on its ID.
-    """
+        """
     output_list = slack_rtm_output
     if output_list and len(output_list) > 0:
         for output in output_list:
             if output and 'text' in output and AT_BOT in output['text']:
                 # return text after the @ mention, whitespace removed
                 return output['text'].split(AT_BOT)[1].strip().lower(), \
-                       output['channel']
+                    output['channel']
     return None, None
 
 
